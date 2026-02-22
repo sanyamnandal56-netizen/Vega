d6ec34e43ffc3db8dac37efe2ced50aeb0871b10
sanyamnandal56-netizen <sanyamnandal56@gmail.com>
2026-02-23 00:07:45 +0530

    Day3

---
+++ day3.py
@@ -0,0 +1,31 @@
+goals = ["get fit", "learn coding", "save money", "sleep better"]
+for goal in goals:
+    print("My goal",goal)
+goals.append("drink water")
+print(goals)
+goals.remove(goals[0])
+print(goals)
+if "get fit" in goals:
+    print("yes")
+else:
+    print("no")
+modules = ["fitness","diet","career","finance"]
+for i in range(2):
+    print(modules[i])
+sleep = [7, 6.5, 8, 5, 7.5, 6, 8]
+bad=0
+for i in sleep:
+    if i<7:
+        bad+=1
+print(bad)    
+workout_log = []
+
+for i in range(3):
+    entry = input("Enter workout " + str(i + 1) + ": ")
+    workout_log.append(entry)
+
+print("--- This Week's Workouts ---")
+for i, entry in enumerate(workout_log, 1):
+    print(str(i) + ".", entry)
+
+
