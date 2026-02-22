33012b1fbcb54101889e3ac247b8deb8620d497d
sanyamnandal56-netizen <sanyamnandal56@gmail.com>
2026-02-22 19:35:07 +0530

    Day2: Made a BMI calculator

--- hello.py
+++ hello.py
@@ -1,9 +1,34 @@
 name=input("Enter you name- ")
-age=input("Enter your age- ")
+age=int(input("Enter your age- "))
 goal=input("Enter your goal-" )
-sleep=input("Enter you sleep goal- ")
-print("--Your Vega Profile--")
+sleep=float(input("Enter you sleep - "))
+gap=8-sleep
+weight=float(input("enter your weight - "))
+height=float(input("enter your height - "))
+BMI=weight/((height/100)**2)
+print("BMI:", round(BMI, 2))
+print("--your Vega Profile--")
 print("Name: ",name)
 print("Age: ",age)
 print("Goal: ",goal)
 print("Sleep: ",sleep)
+if sleep>=8:
+    print("you are doing well")
+elif sleep>=6:
+    print("you are doing good but you can improve by",gap,"hour")
+else:
+    print("you seriously lack sleep you need ",gap,"hours of sleep more")
+
+if age<18:
+    print("go to school Vega will help you build your habits early")
+elif age<=25:
+    print("this is your golden period lets build the skills together")
+else:
+    print("experience is your key use it .")
+if BMI<18.5:
+    print("Underweight — Vega will focus on healthy gain")
+elif BMI<=24.9:
+    print("Healthy range — let's maintain and build")
+else:
+    print("Vega will focus on gradual, sustainable loss")
+
---
+++ types.py
@@ -0,0 +1,8 @@
+name = "arjun"
+age="20"
+number= 7
+y=6.5
+print(type(name))
+print(type(age))
+print(type(number))
+print(type(y))
