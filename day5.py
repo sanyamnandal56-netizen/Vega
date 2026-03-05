460b8c1ad50c3334d18b2194999608925b2e7f18
sanyamnandal56-netizen <sanyamnandal56@gmail.com>
2026-03-05 10:20:43 +0530

    day5:did more practise

--- day5.py
+++ day5.py
@@ -1,9 +1,13 @@
-evensum = 0
-oddsum=0
-for i in range(1,100,1):
-    if i%2==0:
-        evensum = evensum + i
-    else:
-        oddsum=oddsum+i
-print(evensum)
-print(oddsum)
+# evensum = 0
+# oddsum=0
+# for i in range(1,100,1):
+#     if i%2==0:
+#         evensum = evensum + i
+#     else:
+#         oddsum=oddsum+i
+# print(evensum)
+# print(oddsum)
+n= int(input("enter the number - "))
+for i in range(1,n+1):
+    if n%i==0:
+        print(i)
