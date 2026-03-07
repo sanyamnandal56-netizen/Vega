722c19936997a51815b60e647de908853fb65d66
sanyamnandal56-netizen <sanyamnandal56@gmail.com>
2026-03-07 20:00:24 +0530

    more oops

--- oops.py
+++ oops.py
@@ -1,11 +1,30 @@
+# class Factory:
+#     def __init__(self,material,zip,pocekts):
+#         self.material=material
+#         self.zip=zip
+#         self.pockets=pocekts
+#     def show(self):
+#         print(f"the objects are {self.material} , {self.zip} , {self.pockets}")
+# reebok = Factory("leather",2,3)
+# campus = Factory("nylon",3,4)
+# reebok.show()
+# campus.show()
 class Factory:
-    def __init__(self,material,zip,pocekts):
-        self.material=material
-        self.zip=zip
-        self.pockets=pocekts
+    def __init__(self,age):
+        self.age=age
     def show(self):
-        print(f"the objects are {self.material} , {self.zip} , {self.pockets}")
-reebok = Factory("leather",2,3)
-campus = Factory("nylon",3,4)
-reebok.show()
-campus.show()
+        print(f"your age is {self.age}")        
+    @classmethod
+    def hello(cls):
+        print("how are you")
+    @staticmethod
+    def static():
+        print("how are you")
+
+obj = Factory(12)
+obj.show()
+obj.hello()
+obj.static()
+
+        
+
